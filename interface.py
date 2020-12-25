7d92fa4aa27dfe4fb9b22df0d5359e1c9540c355
Yenwen Chen <50478424+kevin880987@users.noreply.github.com>
2020-12-25 15:40:27 +0800

    seperate files

--- interface.py
+++ interface.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jun  1 13:25:22 2020
+Created on Fri Dec 25 10:02:15 2020
 
-@author: user
+@author: kevin
 """
-# https://www.w3schools.com/python/python_mysql_create_database.asp
 
-import pymysql
 import tkinter as tk
 from tkinter import ttk
 from tkinter import font
@@ -594,16 +592,3 @@ class DatabaseInterface(object):
         ButtonInterface(self.connection, self.cursor, self.font_style)
 
             
-# Create the GUI and pass it to our App class
-def main(ip, user, password, database):
-    window= tk.Tk()
-    try:
-        connection = pymysql.connect(ip, user, password, database)
-        cursor = connection.cursor()
-        DatabaseInterface(window, connection, cursor)
-        window.mainloop()
-    finally:
-        cursor.close()
-
-if __name__ == "__main__":
-    main(ip='localhost', user='root', password='red91310', database = 'delivery_db')
