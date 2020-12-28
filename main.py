6afa6ee31a77ca21f605c208ba00910b045c550a
ITingHung <itinghung.tina@gmail.com>
2020-12-28 21:06:57 +0800

    Apply facade pattern

--- main.py
+++ main.py
@@ -4,15 +4,16 @@ import pymysql
 from interface import DatabaseInterface
 
 # Create the GUI and pass it to our App class
-def main(ip, user, password, database):
-    window = tk.Tk()
-    try:
-        connection = pymysql.connect(ip, user, password, database)
-        cursor = connection.cursor()
-        DatabaseInterface(window, connection, cursor)
-        window.mainloop()
-    finally:
-        cursor.close()
+class ConnectWorkbench(object):
+    def __init__(self, ip, user, password, database):
+        window = tk.Tk()
+        try:
+            connection = pymysql.connect(ip, user, password, database)
+            cursor = connection.cursor()
+            DatabaseInterface(window, connection, cursor)
+            window.mainloop()
+        finally:
+            cursor.close()
 
 if __name__ == "__main__":
-    main(ip='localhost', user='root', password='red91310', database = 'delivery_db')
+    ConnectWorkbench(ip='localhost', user='root', password='red91310', database = 'delivery_db')
